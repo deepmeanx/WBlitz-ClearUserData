9bc496e35e512a2937905cd7179fab8e25f309a7
deepmeanx <138305860+deepmeanx@users.noreply.github.com>
2023-09-03 17:35:39 +0300

    Add files via upload

---
+++ ClearWBlitzdata.py
@@ -0,0 +1,41 @@
+#deepmeanx
+import os
+import ctypes
+import shutil
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+def donefile(gamedata_path):
+    print('!Attention! All user data of the game will be DELETED ON THIS SYSTEM, including replays, automatic session and graphics settings (reset to silence.)')
+    print('You are sure?')
+    input()
+    try:
+        shutil.rmtree(gamedata_path)
+        print('Done. All game data has been deleted.')
+    except Exception as e:
+        print('An error occurred while deleting the game data:', e)
+
+def main():
+    if is_admin():
+        ctypes.windll.kernel32.SetConsoleTitleW("Clear WBlitz data")
+        print('This program will help to clear the data of the PC version of the game WoT Blitz or Tanks Blitz')
+        print('Press any key to continue')
+        input()
+        gamedata_path = os.path.expanduser("~/Documents/DAVAProject")
+        expected_path = os.path.expanduser("~/Documents/DAVAProject")
+    
+        if gamedata_path == expected_path:
+            print('Game files found')
+            donefile(gamedata_path)
+        else:
+            print('The game files are not found')
+            exit()
+    else:
+        print("Please, run this program as administrator")
+        exit()
+
+main()
